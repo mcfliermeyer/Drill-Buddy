207c99bf19f6d74abf465e5ee1dacf911324888c
Mark Meyer <mcfliermeyer@gmail.com>
2023-02-17 06:55:41 -0500

    Initial Commit

---
+++ Drill Buddy/Drill_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drill_BuddyApp.swift
+//  Drill Buddy
+//
+//  Created by Mark Meyer on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Drill_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
