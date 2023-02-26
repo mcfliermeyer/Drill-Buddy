5bed5f70a64ab0e32c0d4680ec356f4b256d9a30
mcfliermeyer <mcfliermeyer@gmail.com>
2023-02-25 23:35:45 -0500

    Moved transform of plane detector to SIMD4X4 matrix.

--- Drill Buddy/Custom Views/CustomARView.swift
+++ Drill Buddy/Custom Views/CustomARView.swift
@@ -32,13 +32,18 @@ extension ARView {
         let cameraAnchor = AnchorEntity(.camera)
         cameraAnchor.addChild(model)
         self.scene.addAnchor(cameraAnchor)
-        model.transform.translation = [0, 0, -1]
-//        let transform = simd_float4x4([1,0,0,-1],
-//                                      [0,1,0,-1],
-//                                      [0,0,1,-1],
-//                                      [0,0,0,0])
-//        model.setTransformMatrix(transform, relativeTo: nil)
-        let radians = 90.0 * Float.pi / 180.0
+        
+//        model.transform.translation = [0, 0, -1] code below does the same thing except in the full 4x4
+        
+        let transform = simd_float4x4([1,0,0,0],//these stacks are actually oriented in the wrong way! they are vertical
+                                      [0,1,0,0],
+                                      [0,0,1,0],
+                                      [0,0,-1.2,1])
+        model.setTransformMatrix(transform, relativeTo: nil)
+        
+        //roatating center entity to be flat on users screen
+        let degreesToRotate: Float = 90.0
+        let radians = degreesToRotate * Float.pi / 180.0
         model.transform.rotation *= simd_quatf(angle: radians, axis: SIMD3(x: 1, y: 0, z: 0))
         
         
