9ebd3e7f7a6c9e3e733930cefd73647d637e8c5c
mcfliermeyer <mcfliermeyer@gmail.com>
2023-10-14 18:17:45 -0400

    Start and stop sphere placement and single line drawn between spheres.

--- Drill Buddy/Custom Views/Coordinator.swift
+++ Drill Buddy/Custom Views/Coordinator.swift
@@ -13,29 +13,17 @@ class Coordinator {
     
     var arView: ARView?
     var sceneObserver: Cancellable!
+    var animationObserver: Cancellable!
+    var measureButtonPressAnimationController: AnimationPlaybackController!
     var archNode: ArchNode?
     var raycastResults: [matrix_float4x4] = []
     var recentmeasureButtonPositions: [SIMD3<Float>] = []
+    
     var measureSpheres = [TwoDimensionalSphere(triangleDetailCount: 50, radius: 0.2, color: .white)]
     var measureButton = MeasureButton(radius: 0.25, color: .white, lineWidth: 0.05)
+    var measureLine: AnchorEntity?
     
-    func updateScene(on event: SceneEvents.Update) {
-        
-        guard let arView = arView else { return }
-        guard let archNode = archNode else { return }
-        /**
-         measure button node to move with camera
-         */
-        guard let measureButtonTransform = createmeasureButtonTransform(arView: arView) else { return }
-        measureButton.move(to: measureButtonTransform, relativeTo: AnchorEntity(.camera), duration: 0.01)
-        measureButton.children.forEach( {$0.move(to: measureButtonTransform, relativeTo: AnchorEntity(.camera), duration: 0.01)} )
-        /**
-         arch node to move with raycast results
-         */
-        guard let archNodePositionTransform = createArchNodeTransform(arView: arView) else { return }
-        archNode.move(to: archNodePositionTransform, relativeTo: nil, duration: 0.30)
-        
-    }
+    
     
     func setupUI() {
         
@@ -46,6 +34,94 @@ class Coordinator {
         measureSpheres.last!.generateCollisionShapes(recursive: true)
         
         sceneObserver = arView.scene.subscribe(to: SceneEvents.Update.self) { [unowned self] in self.updateScene(on: $0) }
+        animationObserver = arView.scene.subscribe(to: AnimationEvents.PlaybackCompleted.self) { [unowned self] in self.animationsCompleted(on: $0) }
+        
+    }
+    
+    func updateScene(on event: SceneEvents.Update) {
+        
+        guard let arView = arView else { return }
+        guard let archNode = archNode else { return }
+        /**
+         measure button node to move with camera
+         measure button children should only be one TwoDimensionalSphere to move with same transform
+         */
+        guard let measureButtonTransform = createmeasureButtonTransform(arView: arView) else { return }
+        measureButton.move(to: measureButtonTransform, relativeTo: AnchorEntity(.camera), duration: 0.01)
+        measureButton.children.forEach( {$0.move(to: measureButtonTransform, relativeTo: AnchorEntity(.camera), duration: 0.01)} )
+        /**
+         arch node to move with raycast results
+         */
+        guard let archNodePositionTransform = createArchNodeTransform(arView: arView) else { return }
+        archNode.move(to: archNodePositionTransform, relativeTo: nil, duration: 0.30)
+        
+        animateMeasureLine(arView: arView, archNode: archNode)
+        
+    }
+    
+    func animationsCompleted(on event: AnimationEvents.PlaybackCompleted) {
+        /**
+         check if animation that was completed is for the measure button press  measureButtonPressAnimation
+         */
+        if event.playbackController == self.measureButtonPressAnimationController {
+            
+            guard let arView = arView else { return }
+            guard let archNode = archNode else { return }
+            //we have an odd number of spheres, so we start measuring
+            if  measureSpheres.count > 0 && measureSpheres.count % 2 != 0 {
+                
+                let startSphere = measureSpheres.last!
+                self.measureLine = createLine(between: startSphere.position , archNodePosition: archNode.position)
+                
+                arView.scene.addAnchor(measureLine!)
+                
+                measureSpheres.append(TwoDimensionalSphere(triangleDetailCount: 50, radius: 0.2, color: .white))
+                measureButton.addChild(measureSpheres.last!)
+                measureSpheres.last!.generateCollisionShapes(recursive: true)
+                
+            }
+            else {//we end measuring
+                
+                
+                //we need to try to remove start sphere and end sphere anchors and add them to the line anchor children? perhaps
+                //may need to do calculations to not overload the amount of anchors in realitykit
+                let startSphere = measureSpheres[measureSpheres.count - 2]
+                let endSphere = measureSpheres.last!
+                let line = createLine(between: startSphere.position , archNodePosition: endSphere.position)
+                arView.scene.addAnchor(line)
+                
+                measureSpheres.append(TwoDimensionalSphere(triangleDetailCount: 50, radius: 0.2, color: .white))
+                measureButton.addChild(measureSpheres.last!)
+                measureSpheres.last!.generateCollisionShapes(recursive: true)
+                
+                return
+                
+            }
+            
+        }
+        else {
+            return
+        }
+        
+    }
+    
+    func animateMeasureLine(arView: ARView, archNode: ArchNode) {
+        
+        guard measureLine != nil else { return }
+        //check if we are measuring or not
+        //if measuring, we need to create a line and destroy old line. or change current one?
+        //destroy old line
+        arView.scene.removeAnchor(measureLine!)
+        
+        //check if we are measuring and not initial placement
+        guard measureSpheres.count > 0 && measureSpheres.count % 2 == 0 else { return }
+        let startSphere = measureSpheres[measureSpheres.count - 2]
+        
+        self.measureLine = createLine(between: startSphere.position , archNodePosition: archNode.position)
+        
+        arView.scene.addAnchor(measureLine!)
+        
+        
         
     }
     
@@ -56,7 +132,7 @@ class Coordinator {
         //get camera transform and apply bottomOfScreenTransform to keep the node in the bottom of the screen
         let measureButtonTransform = arView.cameraTransform.matrix * bottomOfScreenTransform.matrix
         
-        //pull out translation/position from controller transform and keep most recent 12 positions
+        //pull out translation/position from controller transform and keep most recent positions
         recentmeasureButtonPositions.append(measureButtonTransform.translation)
         recentmeasureButtonPositions = recentmeasureButtonPositions.suffix(8)
         
@@ -84,7 +160,7 @@ class Coordinator {
         
     }
     
-    @MainActor @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
         
         guard let touchInView = sender?.location(in: arView) else { return }
         
@@ -104,11 +180,31 @@ class Coordinator {
         
         let scaledAndPositioned = positionTransform.matrix * scaleTransform.matrix
         
-        hitEntity.move(to: scaledAndPositioned, relativeTo: nil, duration: 0.4)
+        measureButtonPressAnimationController = hitEntity.move(to: scaledAndPositioned, relativeTo: nil, duration: 0.2)
         
-        measureSpheres.append(TwoDimensionalSphere())
-        measureButton.addChild(measureSpheres.last!)
-        measureSpheres.last!.generateCollisionShapes(recursive: true)
+    }
+    
+    func createLine(between measurePointPosition: SIMD3<Float>, archNodePosition: SIMD3<Float>) -> AnchorEntity {
+        //get midpointbetween first measureSphere and current ArchNode
+        let midpoint = (measurePointPosition + archNodePosition) / 2
+        
+        //create anchor with midposition
+        let anchor = AnchorEntity()
+        anchor.position = midpoint
+        anchor.look(at: measurePointPosition, from: midpoint, relativeTo: nil)
+        
+        let distance = simd_distance(measurePointPosition, archNodePosition)
+        
+        var lineMaterial = UnlitMaterial(color: .white.withAlphaComponent(0.5))
+        let mesh = MeshResource.generateBox(width: 0.005, height: 0.005, depth: distance)
+        
+        let lineEntity = ModelEntity(mesh: mesh, materials: [lineMaterial])
+        
+        lineEntity.position = .init(x: 0, y: 0, z: 0)
+        
+        anchor.addChild(lineEntity)
+        
+        return anchor
         
     }
     
